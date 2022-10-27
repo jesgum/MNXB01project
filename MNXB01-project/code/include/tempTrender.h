1e737af26f76549e61dc38e727d179ef2637e149
Ashar Kamal <ashar.kamal98@gmail.com>
2022-10-28 01:00:50 +0200

    Added error bars to histogram in source file, as well as comments

--- MNXB01-project/code/include/tempTrender.h
+++ MNXB01-project/code/include/tempTrender.h
@@ -12,13 +12,14 @@ using namespace std;
 
 class tempTrender {
 	public:
-	explicit tempTrender(const std::string& filePath); 
+	explicit tempTrender(const std::string& filePath); //Constructor initializes Average daily temperature
+							   // over a period of years, and associated error bars
 	~tempTrender() {} //Destructor
 
-	std::string tempPerDay() const; 
+	std::string tempPerDay() const; //tempPerDay memeber function constructs the histogram with error bars
 	
 	private:
-		//int _Day[365];
-		double _Temp[365];
+		double _Error[365];	//Error assocoiated with measurment. 365 entries for 365 days of the year.
+		double _Temp[365];	//Measurment itself. 29 february (extra day in leap year) removed in filtering process!
 };
 #endif
--- MNXB01-project/code/src/tempTrender.cpp
+++ MNXB01-project/code/src/tempTrender.cpp
@@ -7,37 +7,42 @@ using namespace std;
 
 tempTrender::tempTrender(const std::string& filePath) {
 	std::cout << "The user supplied " << filePath <<" as the path to the data file.\n";
-	ifstream file(filePath);
+	ifstream file(filePath);	//Read filtered file
 	double num1{-1}; int num2{-1};double num3{-1};
 	int i{0};
-	//int j{0};
-	vector<double> Temp{}; vector<double> ATemp{};
+
+	//Temp will contain all temperature meassurments between 1981-1990. ATemp is the daily average termperature between 1981-1990.
+	vector<double> Temp{}; vector<double> ATemp{};	
+
 	while(file >> num1 >> num2 >> num3) {
 		Temp.push_back(num3);
 		if ((i+1)%3==0) {
-			//int k = (i+1)/3-1;
-			//_Day[k]=k;
-			ATemp.push_back((Temp[i]+Temp[i-1]+Temp[i-2])/3);
+			ATemp.push_back((Temp[i]+Temp[i-1]+Temp[i-2])/3);	//3 measurments per day. Average them.
 		};
 		i++;
 	}
-	for (int j=0; j<365; j++) {
-		int a{0};
+	for (int j=0; j<365; j++) {		//Measurments taken over 10 years. Average them.
+		double a{0};
+		double b{0};
 		for (int k=0; k<10; k++) {
 			a+=ATemp[j+k*365];
 		}
 		_Temp[j]=a/10;
+		for (int l=0; l<10; l++) {	//Associated errors
+			b+= pow(ATemp[j+l*365]-_Temp[j], 2);
+		}
+		_Error[j]=sqrt(b/10);
 	}
 }
 
 
-std::string tempTrender::tempPerDay() const {
+std::string tempTrender::tempPerDay() const {	//Constructing histogram
 	TCanvas* c1 = new TCanvas("c1", "TempPerDay canvas", 900, 600);
-	//c1->SetFillColor(42);
 
 	TH1D* PerDayHist = new TH1D("PerDayHist", "Temperature for every day of the year",365, 1, 366);
-	for(int bin=1; bin<366; ++bin) {
+	for(int bin=1; bin<366; ++bin) {	//Fill bins with measurments and error bars
 		PerDayHist->SetBinContent(bin,_Temp[bin-1]);
+		PerDayHist->SetBinError(bin,_Error[bin-1]);
 	}
 	PerDayHist->SetTitle("Temperature for every day of the year");
 	PerDayHist->GetXaxis()->SetTitle("Day");
@@ -45,6 +50,6 @@ std::string tempTrender::tempPerDay() const {
 
 	PerDayHist->Draw();
 	return "Created histogram";
-} //Make a histogram of the average temperature of each day of the year
+} 
 
 
