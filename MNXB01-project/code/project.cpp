040fe34055d428e18c8017f5ff6fd48dbd626cd6
Ashar Kamal <ashar.kamal98@gmail.com>
2022-10-28 01:35:43 +0200

    Small changes / comments to project.cpp and WorkPlan

--- MNXB01-project/code/project.cpp
+++ MNXB01-project/code/project.cpp
@@ -9,16 +9,14 @@ using namespace std;
 
 void TempPerDay() {
 	const string pathToFile = "/home/akamal/git/MNXB01project/MNXB01-project/datasets/smhiLund_8089.csv";  
-
 	tempTrender t(pathToFile); //Instantiate your analysis object
 
-	cout << "cpp file works" << endl;
 	cout << t.tempPerDay() << endl;
 }
 
 void TempOnDay(int Month, int Day) {
 	system("data_cleaner.sh");  //Calls batch script to filer data
-	tempOnDay(Month, Day);		//Remove this
+	tempOnDay(Month, Day);	   //Calls function in tempOnDay.C root file
 }
 
 void Compare(int Month) {
