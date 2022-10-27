662502259e7c6d1adc0e7015defc8fcb263d1d5d
Ashar Kamal <ashar.kamal98@gmail.com>
2022-10-27 12:41:40 +0200

    cleaned up projejt, header and source file, and edited them such that they can be executed in root. Currently the class produces an empty canvas

--- MNXB01-project/code/project.cpp
+++ MNXB01-project/code/project.cpp
@@ -1,13 +1,18 @@
 #include "tempTrender.h"
+//#include "tempTrender.cpp"
+
 #include <string>
+#include <iostream>
+using namespace std;
 
 void project() {
-	const std::string pathToFile = "/home/path/to/file/"; //Put the path to your data file here
+	const string pathToFile = "/home/akamal/git/MNXB01project/MNXB01-project/datasets/Test1";   //"../datasets/smhi_Lund_1965" 
 	tempTrender t(pathToFile); //Instantiate your analysis object
-	
-	//t.tempOnDay(8, 23); //Call some functions that you've implemented
-	//t.tempOnDay(235);
-	//t.tempPerDay();
-	//t.hotCold();
-	//t.tempPerYear(2050);
+
+	cout << "cpp file works" << endl;
+	cout << t.tempPerDay() << endl;
+}
+
+int main() {
+	project();
 }
