cf1a157bc721601695cf1c93e6a35e3ff870bfd7
Zakaria Mohamed <zakimohammed_10@hotmail.co.uk>
2022-10-28 01:16:51 +0200

    Add fitting

--- MNXB01-project/code/hist1.cpp
+++ MNXB01-project/code/hist1.cpp
@@ -1,4 +1,5 @@
 #include "TH1I.h"
+#include "TF1.h"
 #include "TCanvas.h"
 #include <fstream>
 #include <iostream>
@@ -28,17 +29,34 @@ void histo_plot(string input_name) {
 	
 }
 
+
+double Gaussian(double* x, double* par) { //A custom function
+  return par[0]*exp(-0.5*(x[0]*x[0] - 2*x[0]*par[1] +
+    par[1]*par[1])/(par[2]*par[2]));
+}
+
+
 int main()
 {
 	TCanvas *c1 = new TCanvas("c1", "c1");
 
-	histo_plot("maxVals.txt");
+	histo_plot("minVals.txt");
 	c1->Update();
 	histo->SetLineColor(kRed);
 	histo->SetFillColor(kRed);
-	histo_plot("minVals.txt");
+	
+	histo_plot("maxVals.txt");
 	histo->SetLineColor(kBlue);
 	histo->SetFillColor(kBlue);
+
+	TF1* func = new TF1("Gaussian", Gaussian, 90, 300, 3);
+	func->SetParameters(5, 190, 50); //Starting values for fitting
+	func->SetLineColor(kBlack);
+	histo->Fit(func, "QR");
+
+	histo->Draw("SAME");
+
+	cout << "The mean is " << func->GetParameter(1) << endl;
 	c1->Print("demo.pdf");
 	return 0;
 }
